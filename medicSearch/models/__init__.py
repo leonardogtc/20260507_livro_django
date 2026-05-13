c5c2f249bc1183acc57658ec0bb8ed4656f6736b
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-05-13 12:13:37 -0300

    Criando perfis e usuários.

---
+++ medicSearch/models/__init__.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Create your models here.
+
+ROLE_CHOICES = (
+    (1, 'Admin'),
+    (2, 'Médico'),
+    (3, 'Paciente'),
+)
+
+from .Profile import Profile
